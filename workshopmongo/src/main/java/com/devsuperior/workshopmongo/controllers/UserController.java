7bc24728bbb71f68ef8fc4a4d9adaaba763862f6
Carlos Henrique <ichfs9108@gmail.com>
2026-01-09 18:07:17 -0300

    feat: post user

--- workshopmongo/src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ workshopmongo/src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -4,11 +4,10 @@ import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +27,12 @@ public class UserController {
     public ResponseEntity<UserDTO> findAById(@PathVariable String id) {
         UserDTO obj = userService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO obj) {
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
--- workshopmongo/src/main/java/com/devsuperior/workshopmongo/dto/UserDTO.java
+++ workshopmongo/src/main/java/com/devsuperior/workshopmongo/dto/UserDTO.java
@@ -22,4 +22,28 @@ public class UserDTO {
         this.name = entity.getName();
         this.email = entity.getEmail();
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
