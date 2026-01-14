b66ccaf0c4f3c032d91895d064fa5da4285f0c37
Carlos Henrique <ichfs9108@gmail.com>
2026-01-14 02:38:08 -0300

    feat: delete user

--- workshopmongo/src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ workshopmongo/src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,5 +47,11 @@ public class UserController {
     public ResponseEntity<UserDTO> update(@PathVariable String id, @RequestBody UserDTO obj) {
         obj = userService.update(id, obj);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> delete(@PathVariable String id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
